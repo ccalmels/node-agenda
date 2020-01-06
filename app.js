750bd45b66af21a561d6d8ed31dc7c6c2686e4b7
Clement Calmels <clement.calmels@free.fr>
2020-01-06 16:43:19 +0100

    connect to a mongoDB server

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+const uri = 'mongodb://127.0.0.1:27017/';
 
-app.listen(port, function() {
-    console.log("Server is running on port " + port + "...");
+MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to mongoDB server");
+
+    const events = client.db('Agenda').collection('events');
+
+    app.use(express.static('public'));
+
+    app.get('/data', function(req, res) {
+	events.find().toArray(function (err, data) {
+            //set the id property for all client records to the database records, which are stored in ._id field
+            for (var i = 0; i < data.length; i++){
+		data[i].id = data[i]._id;
+		delete data[i]["!nativeeditor_status"];
+            }
+            //output response
+            res.send(data);
+	});
+    });
+
+    app.listen(port, function() {
+	console.log("Server is running on port " + port + "...");
+    });
 });
